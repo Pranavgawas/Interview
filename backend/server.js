d14a408e9728459eef18a41bc9d6c40bfa3fe258
Pranavgawas <pranavgawas1999@gmail.com>
2024-04-09 17:57:09 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import employeeRoutes from "./routes/employeeRoutes.js"
+import path from "path";
+import employeeRoutes from "./routes/employeeRoutes.js";
 import cors from "cors";
 import express from "express";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 
@@ -16,13 +19,19 @@ app.use(cors());
 
 dotenv.config();
 
-app.use('/api/employees', employeeRoutes);
+app.use("/api/employees", employeeRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
-    connectToMongoDB();
-    console.log(`Server is running on port ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
-    res.send("hello world!!");
+  res.send("hello world!!");
 });
